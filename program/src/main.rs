f94c2641ae7a536e85898fafe413247e4cfced6c
Jojo Zhao <jojo@DNa80ee6f.SUNet>
2024-09-12 20:57:14 -0700

    add slow version of resizing program

--- program/src/main.rs
+++ program/src/main.rs
@@ -35,25 +35,76 @@
 sp1_zkvm::entrypoint!(main);
 
 use alloy_sol_types::SolType;
-use fibonacci_lib::{fibonacci, PublicValuesStruct};
+use fibonacci_lib::{fibonacci, PublicValuesStruct, Context};
 use std::cmp::{max, min};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
+
+
 
 pub fn main() {
     // Read an input to the program.
     //
     // Behind the scenes, this compiles down to a system call which handles reading inputs
     // from the prover.
+    let c = sp1_zkvm::io::read::<Context>();
     let image: Vec<u8> = sp1_zkvm::io::read::<Vec<u8>>();
+    let target_image: Vec<u8> = sp1_zkvm::io::read::<Vec<u8>>();
 
-    // Write n to public input
-    //sp1_zkvm::io::commit(&image);
-    sp1_zkvm::io::commit(&image[0]);
-    sp1_zkvm::io::commit(&image[1]);
+    let mut tmp = vec![0u8; c.dst_w as usize * c.src_h as usize];
+    let mut dst = vec![0u8; c.dst_w as usize * c.dst_h as usize];
 
-    print!("image data: {:?}", image);
+    // hard code here
+    const FILTER_BITS: i32 = 14;
+    const FILTER_SCALE: i32 = 1 << FILTER_BITS;
+
+
+    // Horizontal scaling
+    for y in 0..c.src_h as usize {
+        for x in 0..c.dst_w as usize {
+            let src_pos = c.filter_pos[x];
+            let mut val = 0;
+
+            for z in 0..c.filter_size {
+                if src_pos + (z as i32) < c.src_w {
+                    val += image[y * c.src_w as usize + (src_pos as usize + z)] as u32
+                        * c.filter[x * c.filter_size + z] as u32;
+                }
+            }
+
+            tmp[y * c.dst_w as usize + x] = ((val + (1 << (FILTER_BITS - 1))) >> FILTER_BITS) as u8;
+        }
+    }
+
+    // Vertical scaling
+    for y in 0..c.dst_h as usize {
+        for x in 0..c.dst_w as usize {
+            let src_pos = c.v_lum_filter_pos[y];
+            let mut val = 0;
+
+            for z in 0..c.v_lum_filter_size {
+                if src_pos + (z as i32) < c.src_h {
+                    val += tmp[((src_pos + z as i32) as usize) * c.dst_w as usize + x] as u32
+                        * c.v_lum_filter[y * c.v_lum_filter_size + z] as u32;
+                }
+            }
+
+            dst[y * c.dst_w as usize + x] = ((val + (1 << (FILTER_BITS - 1))) >> FILTER_BITS) as u8;
+        }
+    }
+
+    let mut difference: u32 = 0;
+    for i in 0..c.dst_w as usize * c.dst_h as usize{
+        difference += (dst[i] as i16 - target_image[i] as i16).abs() as u32;
+    }
+
+    print!("difference: {:?}", &difference);
+    sp1_zkvm::io::commit(&difference);
+
+    
+
+    // print!("image data: {:?}", image);
 
     // Compute the n'th fibonacci number, using normal Rust code.
     // let mut a = 0;
--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -1,4 +1,8 @@
 use sp1_sdk::{utils, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
+use fibonacci_lib::{Context, load_image_from_file, scale_image};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::path::Path;
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../../elf/riscv32im-succinct-zkvm-elf");
@@ -7,15 +11,57 @@ fn main() {
     // Setup logging.
     utils::setup_logger();
 
-    // Create an input stream and write a hard-coded vector to it.
-    let image: Vec<u8> = vec![1, 2, 3];
-    print!("image data {}, {}", image[0], image[1]);
+    // Todo: how to input from the 
+    // let args: Vec<String> = std::env::args().collect();
+
+    // if args.len() != 7 {
+    //     println!("Usage: {} <input_file> <input_width> <input_height> <output_file> <output_width> <output_height>", args[0]);
+    //     //  sand_19201080_R_channel.txt 1920 1080 custom_bilinear_r.txt 480 270
+    //     return;
+    // }
+
+    // let input_file = "../resources/sand_19201080_R_channel.txt";
+    // let input_width = 1920;
+    // let input_height = 1080;
+    // let output_file = "custom_bilinear_r.txt";
+    // let output_width = 480;
+    // let output_height = 270;
+
+    //fake example
+    let input_file = "../resources/fake_image.txt";
+    let input_width = 120;
+    let input_height = 100;
+    let target_file = "../resources/fake_real_image.txt";
+    let output_width = 60;
+    let output_height = 50;
+    let output_file = "image_output.txt";
+
+    let c = Context::new(input_width, input_height, output_width, output_height).unwrap();
+
+    // Get the Image
+    let image: Vec<u8> =load_image_from_file(input_file);
+    let target_image: Vec<u8> = load_image_from_file(target_file);
+
+    // For test only, generate output file 
+    // let mut output = vec![0u8; (output_width * output_height) as usize];
+    // scale_image(&c, &image, input_width, &mut output, output_width);
+    // let output_path = Path::new(output_file);
+    // let mut output_file = File::create(&output_path).expect("Failed to open output file");
+
+    // for i in 0..output_height {
+    //     for j in 0..output_width {
+    //         write!(output_file, "{} ", output[(i * output_width + j) as usize]).unwrap();
+    //     }
+    //     writeln!(output_file).unwrap();
+    // }
 
     // The input stream that the program will read from using `sp1_zkvm::io::read`.
     // Note that the types of the elements in the input stream must match the types being
     // read in the program.
     let mut stdin = SP1Stdin::new();
+    stdin.write(&c);
     stdin.write(&image);
+    stdin.write(&target_image);
 
     // Create a `ProverClient` method.
     let client = ProverClient::new();
@@ -38,11 +84,12 @@ fn main() {
     // Note that this output is read from values commited to in the program using
     // `sp1_zkvm::io::commit`.
     //let _ = proof.public_values.read::<u32>();
-    let a: u8 = proof.public_values.read::<u8>();
-    let b: u8 = proof.public_values.read::<u8>();
 
-    println!("a: {}", a);
-    println!("b: {}", b);
+    // Calculate the L1 norm here 
+    // Todo: change to range check & speed up
+    let difference: u8 = proof.public_values.read::<u8>();
+
+    println!("difference: {}", difference);
 
     // Verify proof and public values
     client.verify(&proof, &vk).expect("verification failed");
