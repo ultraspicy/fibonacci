81bd3b173cf8e866b2f260c691f79b9bd502b445
ultraspicy <element.no.88@gmail.com>
2024-09-23 08:40:21 -0700

    feat: Add proof aggregation module template

--- video-resizing/script/src/bin/main.rs
+++ video-resizing/script/src/bin/main.rs
@@ -1,26 +1,32 @@
+use lib::{load_image_from_file, Context};
 use sp1_sdk::{utils, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
-use lib::{Context, load_image_from_file};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../../elf/riscv32im-succinct-zkvm-elf");
+
+const FRAME_NUM: usize = 10;
+const input_width: i32 = 192;
+const input_height: i32 = 108;
+const output_width: i32 = 96;
+const output_height: i32 = 54;
 
 fn main() {
     // Setup logging.
     utils::setup_logger();
 
+    for i in 1..=FRAME_NUM {
+        let filename = format!("../resources/ffmpeg_bilinear_output/output_{:03}.png", i);
+    }
+
     //fake example
     let input_file = "../resources/fake_image.txt";
-    let input_width = 120;
-    let input_height = 100;
     let target_file = "../resources/fake_real_image.txt";
-    let output_width = 60;
-    let output_height = 50;
     let output_file = "image_output.txt";
 
     let context = Context::new(input_width, input_height, output_width, output_height).unwrap();
 
     // Get the Image
-    let image: Vec<u8> =load_image_from_file(input_file);
+    let image: Vec<u8> = load_image_from_file(input_file);
     let target_image: Vec<u8> = load_image_from_file(target_file);
 
     // The input stream that the program will read from using `sp1_zkvm::io::read`.
@@ -52,11 +58,15 @@ fn main() {
     // `sp1_zkvm::io::commit`.
     //let _ = proof.public_values.read::<u32>();
 
-    // Calculate the L1 norm here 
+    // Calculate the L1 norm here
     // Todo: change to range check & speed up
-    let difference: u8 = proof.public_values.read::<u8>();
 
-    println!("difference: {}", difference);
+    // Note: if commit a Vec<> the proof time will be very long
+    // for quick verification, use a usize to sum it up
+    // TODO: investigate why commit to Vec<> will hurt the performance
+    let difference: Vec<usize> = proof.public_values.read::<Vec<usize>>();
+    println!("difference: {}", difference[0]);
+    // let difference: usize = proof.public_values.read::<usize>();
 
     // Verify proof and public values
     client.verify(&proof, &vk).expect("verification failed");
