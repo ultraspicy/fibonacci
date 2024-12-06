e6dedb07eb7ab583698ea04d80d16b56d242f221
sashafrolov <sasha07974@gmail.com>
2024-12-06 13:41:35 -0500

    Temp gblur testing commit

--- video-resizing/script/src/bin/main.rs
+++ video-resizing/script/src/bin/main.rs
@@ -1,14 +1,14 @@
-use lib::{load_image_from_file, ResizeContext};
+use lib::{load_image_from_file, BlurContext, ResizeContext};
 use sp1_sdk::{utils, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../../elf/riscv32im-succinct-zkvm-elf");
 
 const _FRAME_NUM: usize = 10;
-const INPUT_WIDTH: i32 = 192;
-const INPUT_HEIGHT: i32 = 108;
-const OUTPUT_WIDTH: i32 = 48;
-const OUTPUT_HEIGHT: i32 = 27;
+const INPUT_WIDTH: i32 = 400;
+const INPUT_HEIGHT: i32 = 400;
+const OUTPUT_WIDTH: i32 = 400;
+const OUTPUT_HEIGHT: i32 = 400;
 
 fn main() {
     // Setup logging.
@@ -19,8 +19,9 @@ fn main() {
     let target_file = "../../resources/ffmpeg_resized_frames_48_27/output_001_R.txt"; //"../../resources/ffmpeg_resized_frames_48_27/output_001_R.txt";
                                                                                       //let output_file = "image_output.txt";
 
-    let context =
-        ResizeContext::new(INPUT_WIDTH, INPUT_HEIGHT, OUTPUT_WIDTH, OUTPUT_HEIGHT).unwrap();
+    let context = BlurContext::new(10f32, 10f32, 1, INPUT_WIDTH, INPUT_HEIGHT).unwrap();
+    // let context =
+    // ResizeContext::new(INPUT_WIDTH, INPUT_HEIGHT, OUTPUT_WIDTH, OUTPUT_HEIGHT).unwrap();
 
     // Get the Image
     let image: Vec<u8> = load_image_from_file(input_file);
