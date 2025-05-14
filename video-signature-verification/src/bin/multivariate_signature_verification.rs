e213f92f7d6011256a3826db2dd22ebc557dfc43
sashafrolov <sasha07974@gmail.com>
2025-05-14 10:43:10 -0400

    Adding some sig verification stuff that seems to not have been pushed to github

--- video-signature-verification/src/bin/multivariate_signature_verification.rs
+++ video-signature-verification/src/bin/multivariate_signature_verification.rs
@@ -1,4 +1,4 @@
-// Code largely adapted from: https://github.com/Pratyush/hekaton-system/blob/main/cp-groth16/benches/bench.rs
+// This code is also heavily adapted from: https://github.com/Pratyush/hekaton-system/blob/main/cp-groth16/benches/bench.rs
 // The code in that file did like 90% of what I needed already.
 
 use ark_bls12_381::{Bls12_381 as E, Fr as F};
@@ -10,6 +10,7 @@ use ark_cp_groth16::{
 };
 use ark_ff::{Field, One, PrimeField, UniformRand};
 use ark_groth16::r1cs_to_qap::LibsnarkReduction as QAP;
+use ark_poly::{DenseMultilinearExtension, MultilinearExtension, Polynomial};
 use ark_r1cs_std::{
     eq::EqGadget,
     fields::fp::FpVar,
@@ -20,28 +21,30 @@ use ark_relations::{
     r1cs::{ConstraintSystemRef, SynthesisError},
 };
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ark_std::log2;
 use ark_std::rand::Rng;
+use rand::SeedableRng;
 use sha2::{Digest, Sha256};
 use std::any::type_name;
 
-const MESSAGE_LENGTH: usize = 1 << 22;
+const MESSAGE_LENGTH: usize = 1 << 15;
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 /// A multistage circuit
-/// Stage 1. Witness the coefficients of the polynomial.
-/// Stage 2. Prove that the polynomial evaluates to a given value at a random point derived in an F-S way from the coefficients.
+/// Stage 1. Witness the evaluations on Boolean Hypercube of polynomial.
+/// Stage 2. Prove evaluation at a random point chosen in a Fiat-Shamir way.
 #[derive(Clone)]
 struct PolyEvalCircuit {
-    // A polynomial that is committed in stage 0. Constrained to be monic.
+    // A polynomial that is committed in stage 0.
     pub polynomial: Vec<F>,
 
     // The variable corresponding to `polynomial` that is generated after stage 0.
     pub polynomial_var: Option<Vec<FpVar<F>>>,
 
-    // The evaluation point for the polynomial. Derived via Fiat-Shamir + Commit and Prove.
-    pub point: Option<F>,
+    // The evaluation point for the polynomial (a vector of log N points in multilinear case).
+    pub point: Option<Vec<F>>,
 
     // The evaluation of `self.polynomial` at `self.root`.
     pub evaluation: Option<F>,
@@ -58,51 +61,62 @@ impl PolyEvalCircuit {
     }
 
     fn rand(mut rng: impl Rng) -> Self {
-        // Sample a random monic polynomial of degree MESSAGE_LENGTH - 1
-        let degree = MESSAGE_LENGTH - 1;
+        // Sample a random multilinear polynomial of the correct degree.
+        let degree = MESSAGE_LENGTH;
         let mut polynomial = (0..degree).map(|_| F::rand(&mut rng)).collect::<Vec<_>>();
-        polynomial.push(F::one());
         Self::new(polynomial)
     }
 
-    fn add_point(&mut self, point: F) {
+    fn add_point(&mut self, point: Vec<F>) {
         use ark_std::Zero;
-        self.point = Some(point);
-        self.evaluation = Some(
-            self.polynomial
-                .iter()
-                .enumerate()
-                .fold(F::zero(), |acc, (i, c)| acc + c * &point.pow(&[i as u64])),
-        );
+        self.point = Some(point.clone());
+        let mle =
+            DenseMultilinearExtension::from_evaluations_vec(point.len(), self.polynomial.clone());
+
+        self.evaluation = mle.evaluate(&point);
     }
 
     fn stage_0(&mut self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
         let polynomial_var = self
             .polynomial
             .iter()
-            .map(|c| FpVar::new_witness(ns!(cs, "coeff"), || Ok(c)))
+            .map(|c| FpVar::new_witness(ns!(cs, "evaluation"), || Ok(c)))
             .collect::<Result<Vec<_>, _>>()?;
-        polynomial_var
-            .last()
-            .unwrap()
-            .enforce_equal(&FpVar::one())?;
         self.polynomial_var = Some(polynomial_var);
 
         Ok(())
     }
 
     fn stage_1(&mut self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
-        let point = FpVar::new_input(ns!(cs, "point"), || Ok(self.point.unwrap()))?;
+        let num_vars = log2(self.polynomial.len()) as usize;
+        let mut point = Vec::new();
+        for i in 0..num_vars {
+            point.push(FpVar::new_input(ns!(cs, "point"), || {
+                Ok(self.point.as_ref().unwrap()[i])
+            })?);
+        }
+
         let evaluation = FpVar::new_input(ns!(cs, "point"), || Ok(self.evaluation.unwrap()))?;
 
-        // This way of implementing evaluation takes half the coefficients as what was in Hekaton!
-        let claimed_eval = self
-            .polynomial_var
-            .as_ref()
+        let mut eq_polynomial_evaluation_stages = vec![vec![FpVar::one()]];
+        // Progressively build up eq polynomial evaluations.
+        for p in point {
+            let mut next_stage = Vec::new();
+            for e in eq_polynomial_evaluation_stages.last().unwrap() {
+                next_stage.push((FpVar::one() - &p) * e);
+            }
+            for e in eq_polynomial_evaluation_stages.last().unwrap() {
+                next_stage.push(&p * e);
+            }
+            eq_polynomial_evaluation_stages.push(next_stage);
+        }
+
+        let claimed_eval = eq_polynomial_evaluation_stages
+            .last()
             .unwrap()
             .iter()
-            .rev()
-            .fold(FpVar::zero(), |acc, x| (acc * &point) + x);
+            .zip(self.polynomial_var.as_ref().unwrap())
+            .fold(FpVar::zero(), |acc, (x, y)| acc + x * y);
 
         // Assert that it's a root
         claimed_eval.enforce_equal(&evaluation)?;
@@ -131,6 +145,12 @@ impl MultiStageConstraintSynthesizer<F> for PolyEvalCircuit {
 }
 
 fn main() {
+    assert!(
+        MESSAGE_LENGTH.is_power_of_two(),
+        "Circuit does not yet support non-power of 2 messages",
+    );
+    let num_vars = log2(MESSAGE_LENGTH) as usize;
+
     let mut rng = ark_std::test_rng();
     let circuit = PolyEvalCircuit::rand(&mut rng);
 
@@ -139,7 +159,7 @@ fn main() {
         let mut circuit = circuit.clone();
         let mut cs = MultiStageConstraintSystem::default();
         circuit.generate_constraints(0, &mut cs).unwrap();
-        let point = F::rand(&mut rng);
+        let point = (0..num_vars).map(|_| F::rand(&mut rng)).collect();
         circuit.add_point(point);
         circuit.generate_constraints(1, &mut cs).unwrap();
         // assert!(cs.is_satisfied().unwrap());
@@ -154,7 +174,6 @@ fn main() {
         start.elapsed().as_secs_f64()
     );
 
-    // Generate a commitment to coefficients
     let mut rng = ark_std::test_rng();
     let mut cb = CommitmentBuilder::<_, E, QAP>::new(circuit, &pk);
     let start = ark_std::time::Instant::now();
@@ -173,30 +192,32 @@ fn main() {
 
     let mut hasher = Sha256::new();
     hasher.update(&compressed_comm);
-    let result = hasher.finalize();
+    let seed = hasher.finalize();
 
-    let point = F::from_be_bytes_mod_order(&result);
-    cb.circuit.add_point(point); // This takes surprisingly long when there are a lot of coefficients
+    let mut cp_rng = rand::rngs::StdRng::from_seed(seed.into());
+    let point = (0..num_vars)
+        .map(|_| F::rand(&mut cp_rng))
+        .collect::<Vec<_>>();
+    cb.circuit.add_point(point.clone());
     println!(
         "generating point from commit and prove data: {} s",
         start.elapsed().as_secs_f64()
     );
 
-    // Actual proof.
     let start = ark_std::time::Instant::now();
-    let inputs = [point, cb.circuit.evaluation.unwrap()];
+    let mut inputs = point.clone();
+    inputs.push(cb.circuit.evaluation.unwrap());
     let proof = cb.prove(&[comm], &[rand], &mut rng).unwrap();
     println!(
         "proving time for BLS12-381: {} s",
         start.elapsed().as_secs_f64()
     );
 
-    // Verify
     let start = ark_std::time::Instant::now();
+    // Verify
     let pvk = prepare_verifying_key(&pk.vk());
     assert!(verify_proof(&pvk, &proof, &inputs).unwrap());
 
-    // Verify that eval point was derived correctly.
     let mut compressed_comm = Vec::new();
     proof.ds[0]
         .serialize_compressed(&mut compressed_comm)
@@ -204,10 +225,13 @@ fn main() {
 
     let mut hasher = Sha256::new();
     hasher.update(&compressed_comm);
-    let result = hasher.finalize();
+    let seed = hasher.finalize();
 
-    let claimed_point = F::from_be_bytes_mod_order(&result);
-    assert!(claimed_point == inputs[0]);
+    let mut cp_rng = rand::rngs::StdRng::from_seed(seed.into());
+    let claimed_point = (0..num_vars)
+        .map(|_| F::rand(&mut cp_rng))
+        .collect::<Vec<_>>();
+    assert!(claimed_point == inputs[0..num_vars]);
 
     println!(
         "verification time for BLS12-381: {} s",
